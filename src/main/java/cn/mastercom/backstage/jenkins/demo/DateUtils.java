2e53805df766f4100699a7fa8441d6a1f26cff72
huangyulong <1410914605@qq.com>
2020-03-31 13:41:33 +0800

    demo测试

--- src/main/java/cn/mastercom/backstage/jenkins/demo/DateUtils.java
+++ src/main/java/cn/mastercom/backstage/jenkins/demo/DateUtils.java
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateUtils {
-	private static  SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	private static  SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd");
 	public static String getDateString(Date date){
 		return dateFormat.format(date);
 	}
